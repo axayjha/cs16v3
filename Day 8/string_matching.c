2dd2ecd5c45b5faad4cc0a97afba74f6864081bc
Akshay Jha <akshayjha@live.in>
2017-10-24 20:28:55 +0530

    Add files via upload

---
+++ Day 8/string_matching.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int is_in(const char str[], const char substr[])
+{	
+	for(int i=0, n=strlen(str), m=strlen(substr); i<n-m; i++)
+		for(int j=0; j<m; j++)
+			if(str[i+j]==substr[j]) if(j==m-1) return i;
+	return -1;
+}
+
+int main()
+{
+	printf("%d\n", is_in("St. Thomas' College", "Tho"));
+	printf("%d\n", is_in("St. Thomas' College", "education"));
+}
